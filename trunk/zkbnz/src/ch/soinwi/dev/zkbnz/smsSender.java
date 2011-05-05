99ef20fd0e02fb6f35286a8f68adcc2c6a263793
applican@gmail.com <applican@gmail.com@21b3ff25-268b-fd02-6634-0b0cd52b043d>
2011-05-05 23:31:08 +0000

    first upload... hmm..

---
+++ trunk/zkbnz/src/ch/soinwi/dev/zkbnz/smsSender.java
@@ -0,0 +1,16 @@
+package ch.soinwi.dev.zkbnz;
+
+import android.telephony.SmsManager;
+
+public class smsSender {
+	
+	public void send_sms(String content, String phone_nr)
+	{
+		SmsManager smsman = SmsManager.getDefault();
+		
+		smsman.sendTextMessage(phone_nr, null, content, null, null);
+
+
+	}
+
+}

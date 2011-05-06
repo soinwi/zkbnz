f4eaaaabbc0aace58179d1495f4e126ae774bdb8
applican@gmail.com <applican@gmail.com@21b3ff25-268b-fd02-6634-0b0cd52b043d>
2011-05-06 08:29:01 +0000

    Added code-comments added larger button after sms sending ends app greys out send button if no sms-sending required ensure that each time the app loads the messages ar loaded as well

--- trunk/zkbnz/src/ch/soinwi/dev/zkbnz/checkSms.java
+++ trunk/zkbnz/src/ch/soinwi/dev/zkbnz/checkSms.java
@@ -17,21 +17,23 @@ import java.util.Vector;
 
 public class checkSms {
 	 
-	private Date lastSmsDate;
-	private String lastSmsBody = null;
-	private Calendar lastValidUntil = Calendar.getInstance();
-	private Activity a;
+	private Date lastSmsDate;										//Deprecated
+	private String lastSmsBody = null;								//contains last sms's body from given number
+	private Calendar lastValidUntil = Calendar.getInstance();		//calendar containt valid until date from last sms if available
+	private Activity a;												//activity to set as parent
 	
 	
 	public checkSms(Activity a)
 	{
-		this.a = a;
+		this.a = a;		//set a to activity
 		
+		
+		//Query the content provider for Sms from 988, get date and body
 		Uri inboxSmsUri = Uri.parse("content://sms/inbox");
 		String[] columns = {"body","date"};
 		Cursor inboxCursor = a.getContentResolver().query(inboxSmsUri, columns, "address = '988'",null, "date DESC");
 		
-		if(inboxCursor.moveToFirst() )
+		if(inboxCursor.moveToFirst() ) //get first element (the newest one)
 		{
 			
 			int dateCol = inboxCursor.getColumnIndex("date");
@@ -48,11 +50,11 @@ public class checkSms {
 		}
 	}
 	
-	public boolean attemptSend()
+	public boolean attemptSend()		//check if should send sms
 	{
 		
 		
-		if(! oldSmsValid())
+		if(! oldSmsValid())				//if no other, older sms exists
 		{
 			
 			smsSender s = new smsSender();
@@ -67,7 +69,7 @@ public class checkSms {
 		return false;
 	}
 	
-	public boolean isValidDay()
+	public boolean isValidDay()	//returns if today is friday (http://www.youtube.com/watch?v=CD2LRROpph0), saturday or sunday
 	{
 		Calendar rightNow = Calendar.getInstance();
 		
@@ -86,58 +88,58 @@ public class checkSms {
 		
 	}
 	
-	public	 boolean oldSmsValid()
+	public	 boolean oldSmsValid()	//checks if an older sms is still valid
 	{
-		Pattern matcherPattern = Pattern.compile(".*Bis.*(\\d\\d).(\\d\\d).(\\d\\d)\\s(\\d\\d):(\\d\\d)");
-		if(lastSmsBody==null)
+		Pattern matcherPattern = Pattern.compile(".*Bis.*(\\d\\d).(\\d\\d).(\\d\\d)\\s(\\d\\d):(\\d\\d)");	//regex pattern
+		if(lastSmsBody==null)	//if there is no older sms
 		{
 			return false;
 		}
-		Matcher match = matcherPattern.matcher(lastSmsBody);
-		Vector<String> date_matches = new Vector<String>();
+		Matcher match = matcherPattern.matcher(lastSmsBody);	//match regex
+		Vector<String> date_matches = new Vector<String>();		//contains match results
 		
-		if( match.find() )
+		if( match.find() )		//if there is a match result (happens, if the last sms is a ticket. yet has to be determined wheter it is still valid)
 		{
 		
-			int i=1;
-			while(i<=5)
+			int i=1;		//start at element 1, 0 contains whole match string
+			while(i<=5)		//get first 5 elements
 			{
-				date_matches.add(match.group(i));
+				date_matches.add(match.group(i));		//insert to vector
 				i++;
 				//i=2;
 			}
 
+																				//get logical values until when the ticket is valid
+			int day = Integer.parseInt(date_matches.elementAt(0) );				//
+			int month = Integer.parseInt(date_matches.elementAt(1) ) - 1;		//months in java calendar start with 0, not 1. so substract one...
+			int year = Integer.parseInt(date_matches.elementAt(2) ) + 100;		//0 as year means 1900, so add 100 (provides 2 digit year
+			int hour = Integer.parseInt(date_matches.elementAt(3) );			//hour
+			int min = Integer.parseInt(date_matches.elementAt(4) );				//min
 
-			int day = Integer.parseInt(date_matches.elementAt(0) );
-			int month = Integer.parseInt(date_matches.elementAt(1) ) - 1;
-			int year = Integer.parseInt(date_matches.elementAt(2) ) + 100;
-			int hour = Integer.parseInt(date_matches.elementAt(3) );
-			int min = Integer.parseInt(date_matches.elementAt(4) );
 
-
-			lastValidUntil.setTime( new Date(year, month, day, hour, min	) );
-			Calendar dateNow = Calendar.getInstance();
+			lastValidUntil.setTime( new Date(year, month, day, hour, min	) );	//define time
+			Calendar dateNow = Calendar.getInstance();								//get current time
 
 			//String group0 = null;
 
-			long neues = dateNow.get(Calendar.DAY_OF_MONTH);
+			/*long neues = dateNow.get(Calendar.DAY_OF_MONTH);
 			long neuesmonth = dateNow.get(Calendar.MONTH);
 			long neuesyear = dateNow.get(Calendar.YEAR);
 
 			long altes = lastValidUntil.get(Calendar.DAY_OF_MONTH);
 			long altesmonth = lastValidUntil.get(Calendar.MONTH);
-			long altesyear = lastValidUntil.get(Calendar.YEAR);
+			long altesyear = lastValidUntil.get(Calendar.YEAR);*/
 
 
-			if( ((int)dateNow.getTime().compareTo(lastValidUntil.getTime())) >= 0 )
+			if( ((int)dateNow.getTime().compareTo(lastValidUntil.getTime())) >= 0 ) //if current date/time is bigger (later) than the expired-date of the ticket, return false
 			{
-				return false;
-				//group0 = "ungültig";
+				return false;					//no valid ticket available
+				//group0 = "ungültig"; 
 
 			}
 			else
 			{
-				return true;
+				return true;					//valid ticket available
 				//group0 = "gültig";
 			}
 		}
@@ -163,7 +165,7 @@ public class checkSms {
 		t.show();*/
 	}
 	
-	public String getLastText()
+	public String getLastText()		//return the text of the last sms...
 	{
 		return lastSmsBody;
 	}
--- trunk/zkbnz/src/ch/soinwi/dev/zkbnz/zkbnz.java
+++ trunk/zkbnz/src/ch/soinwi/dev/zkbnz/zkbnz.java
@@ -17,24 +17,31 @@ public class zkbnz extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button sender = (Button)findViewById(R.id.hellobutton);
-        
-       // button_listener bl = new button_listener();
-        
+        Button sender = (Button)findViewById(R.id.hellobutton);		//enable events on button        
         sender.setOnClickListener(this);
         
-        c = new checkSms(this);
+        c = new checkSms(this);				//sms checker instance to privide functionality like daychecking
         
-        if( !c.isValidDay() )
+        if( !c.isValidDay() )				//if it's not a valid day (friday, saturday, sunday morning)
         {
-        	sender.setText("Kein gültiger Tag, trotzdem senden?");
+        	sender.setText("Kein gültiger Tag, trotzdem senden?");		//Warn user
         }
         
-        if(c.oldSmsValid() )
+        if(c.oldSmsValid() )				//if there already is a valid message int the inbox
         {
-        	TextView text = (TextView)findViewById(R.id.messagetext);
+        	TextView text = (TextView)findViewById(R.id.messagetext);		//show it
         	text.setText(c.getLastText() );
+        	
+        	sender.setEnabled(false);										//and disable button
         }
+    }
+    
+    @Override
+    public void onPause()		//when app gets out of focus
+    {
+    	super.onPause();
+    	
+    	this.finish();			//quit it to ensure that each time it is displayed it refetches the messages
     }
 
     public checkSms getChecker()
@@ -45,12 +52,12 @@ public class zkbnz extends Activity implements OnClickListener {
     public void onClick(View v)
 	{
 		
-		c = new checkSms(this);
+		c = new checkSms(this);			//sms checker instance
     	Button caller = (Button)v;
 			
-		if(! c.attemptSend() )
+		if(! c.attemptSend() )			//if sending-attempt returns false, that means ther was no sms sent because there is already an existing ticket
 		{
-			Toast t = Toast.makeText(v.getContext(), "Es ist bereits ein gültiges Ticket vorhanden", Toast.LENGTH_LONG);
+			Toast t = Toast.makeText(v.getContext(), "Es ist bereits ein gültiges Ticket vorhanden", Toast.LENGTH_LONG);		//inform user about that
 			t.show();
 			
 			TextView text = (TextView)findViewById(R.id.messagetext);
@@ -59,11 +66,11 @@ public class zkbnz extends Activity implements OnClickListener {
 		
 		else
 		{
-			TextView text = (TextView)findViewById(R.id.messagetext);
+			TextView text = (TextView)findViewById(R.id.messagetext);		//otherwise change button text
 			text.setText("" );
 			caller.setText("SMS gesendet");
 			
-			this.finish();
+			this.finish();							//and finisch activity
 		}
 	}
 
